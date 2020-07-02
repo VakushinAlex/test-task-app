d52fdb5a87be1b20e87e95edd1ea465dd1c89bc8
404 <404@intersvyaz.net>
2020-07-02 16:30:28 +0500

    Updates

---
+++ precache-manifest.789418c358820793ae6f670f77878d00.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8181fc3c2e087ce80a8fe309f7df9d77",
+    "url": "/test-task-app/index.html"
+  },
+  {
+    "revision": "93cf0b0881fb8f3ce205",
+    "url": "/test-task-app/static/css/2.9da3a440.chunk.css"
+  },
+  {
+    "revision": "5d7bb97bb8a2df3b2a41",
+    "url": "/test-task-app/static/css/main.37430e70.chunk.css"
+  },
+  {
+    "revision": "93cf0b0881fb8f3ce205",
+    "url": "/test-task-app/static/js/2.f86c56ad.chunk.js"
+  },
+  {
+    "revision": "577adbd8d67426cff363294b97e054b4",
+    "url": "/test-task-app/static/js/2.f86c56ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d7bb97bb8a2df3b2a41",
+    "url": "/test-task-app/static/js/main.47e9cd34.chunk.js"
+  },
+  {
+    "revision": "0f841c8ff2148e10510f",
+    "url": "/test-task-app/static/js/runtime-main.2b229d83.js"
+  },
+  {
+    "revision": "e3c3af7fe1f7f2db3a9d517b1ee16a3c",
+    "url": "/test-task-app/static/media/angle-left-solid.e3c3af7f.svg"
+  },
+  {
+    "revision": "4c47b68efb72e5f94fcde96837f8e4cb",
+    "url": "/test-task-app/static/media/bars-solid.4c47b68e.svg"
+  },
+  {
+    "revision": "6ee0b54d504211b34dc41a60d6bb7540",
+    "url": "/test-task-app/static/media/check-solid.6ee0b54d.svg"
+  },
+  {
+    "revision": "1866855a43bea537aa3633ad35b31c47",
+    "url": "/test-task-app/static/media/icons8-business_contact.1866855a.svg"
+  },
+  {
+    "revision": "f73666c5bd3955be4af1d6500acf515a",
+    "url": "/test-task-app/static/media/icons8-calendar.f73666c5.svg"
+  },
+  {
+    "revision": "859054021667cd36473135ed30759131",
+    "url": "/test-task-app/static/media/icons8-collaboration.85905402.svg"
+  },
+  {
+    "revision": "d4f774beaf0965fa3fd2f31a5088e83f",
+    "url": "/test-task-app/static/media/icons8-dashboard.d4f774be.svg"
+  },
+  {
+    "revision": "354c4b57aa4b346a53ad1ebc75044fd2",
+    "url": "/test-task-app/static/media/icons8-inbox.354c4b57.svg"
+  },
+  {
+    "revision": "7fadb9bfb3fdc40d6ba2c6266b9bf009",
+    "url": "/test-task-app/static/media/icons8-live_folder.7fadb9bf.svg"
+  },
+  {
+    "revision": "370c239f4447c981fc5ee583c550e7a1",
+    "url": "/test-task-app/static/media/icons8-number_pad.370c239f.svg"
+  },
+  {
+    "revision": "cc7b93beeb848018dba676431990ea10",
+    "url": "/test-task-app/static/media/icons8-todo_list.cc7b93be.svg"
+  },
+  {
+    "revision": "f3594b411888b3fe2ec5e80ce4340fad",
+    "url": "/test-task-app/static/media/icons8-user_group_man_man.f3594b41.svg"
+  }
+]);
